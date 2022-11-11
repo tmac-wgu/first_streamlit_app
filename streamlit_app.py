455d0741d86cee2adb773c37903995ccfe2a4815
Tim McKean <74784416+tmac-wgu@users.noreply.github.com>
2022-11-11 09:17:42 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,8 +49,6 @@ try:
 except URLError as e:
   streamlit.error()
 
-streamlit.stop()
-
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * FROM pc_rivery_db.public.fruit_load_list")
