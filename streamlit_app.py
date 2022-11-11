9b848eb6a7ab58a83979bdbe546be9d340cfee36
Tim McKean <74784416+tmac-wgu@users.noreply.github.com>
2022-11-11 09:26:47 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ def insert_row_snowflake(new_fruit):
         return "Thanks for adding " + new_fruit
     
 def get_fruit_load_list():
-    with my_cnx.cursor() as my_cur
+    with my_cnx.cursor() as my_cur:
     my_cur.execute("SELECT * FROM pc_rivery_db.public.fruit_load_list")
     my_data_rows = my_cur.fetchall()
     return my_data_rows
